bfea5dcf14fe2782ed33254d07ed9408f0f5a724
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-03-23 21:43:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images=["father.PNG","mother.PNG","bedantika.jpg","Bihan.PNG","Kaku.PNG","Kakimoni.PNG"];
-var names=["Biplab Kumar Roy","Kalpana Roy","Bedantika Roy","Bihan Roy","Biman Roy","Ritubarna Roy"];
+var names=["He is Biplab Kumar Roy.He is my father.","She is my mother kalpana Roy.","This is me Bedantika Roy.","He is my brother Bihan Roy.","He is Biman Roy my uncle.","She is Ritubarna Roy my aunt."];
 var i=0;
 function next_image(){
 document.getElementById("reason_text").innerHTML=names[i];
